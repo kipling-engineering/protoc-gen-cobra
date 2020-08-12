c6aea8d99a59d1d88ec0ca9df15c37b402a551f6
Nathan Baulch <nathan.baulch@gmail.com>
2020-08-12 15:16:56 +1000

    Workaround for yaml omitempty

--- iocodec/yaml/init.go
+++ iocodec/yaml/init.go
@@ -3,6 +3,7 @@ package yaml
 import (
 	"io"
 
+	"github.com/mitchellh/mapstructure"
 	"gopkg.in/yaml.v2"
 
 	"github.com/NathanBaulch/protoc-gen-cobra/client"
@@ -19,7 +20,18 @@ func decoderMaker(r io.Reader) iocodec.Decoder {
 }
 
 func encoderMaker(w io.Writer) iocodec.Encoder {
-	e := yaml.NewEncoder(w)
-	defer e.Close()
-	return e.Encode
+	return func(v interface{}) error {
+		// workaround: yaml encoder doesn't honor json tags so pre-preprocess with mapstructure first
+		m := make(map[string]interface{})
+		cfg := &mapstructure.DecoderConfig{Result: &m, TagName: "json"}
+		if dec, err := mapstructure.NewDecoder(cfg); err != nil {
+			return err
+		} else if err := dec.Decode(v); err != nil {
+			return err
+		}
+
+		e := yaml.NewEncoder(w)
+		defer e.Close()
+		return e.Encode(m)
+	}
 }
