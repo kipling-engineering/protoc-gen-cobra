16865529243ed518b1de16e2e276a6c7439fb5b6
Nathan Baulch <nathan.baulch@gmail.com>
2023-02-11 13:04:23 +1100

    Improve enum type safety

--- flag/enum.go
+++ flag/enum.go
@@ -9,12 +9,12 @@ import (
 	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
-func EnumVar[T ~int32](fs *pflag.FlagSet, p *T, name, usage string) {
+func EnumVar[T protoreflect.Enum](fs *pflag.FlagSet, p *T, name, usage string) {
 	v := fs.String(name, "", usage)
 	WithPostSetHookE(fs, name, func() (err error) { *p, err = ParseEnumE[T](*v); return })
 }
 
-func EnumPointerVar[T ~int32](fs *pflag.FlagSet, p **T, name, usage string) {
+func EnumPointerVar[T protoreflect.Enum](fs *pflag.FlagSet, p **T, name, usage string) {
 	v := fs.String(name, "", usage)
 	WithPostSetHookE(fs, name, func() error {
 		if e, err := ParseEnumE[T](*v); err != nil {
@@ -26,24 +26,34 @@ func EnumPointerVar[T ~int32](fs *pflag.FlagSet, p **T, name, usage string) {
 	})
 }
 
-func EnumSliceVar[T ~int32](fs *pflag.FlagSet, p *[]T, name, usage string) {
+func EnumSliceVar[T protoreflect.Enum](fs *pflag.FlagSet, p *[]T, name, usage string) {
 	SliceVar[T](fs, ParseEnumE[T], p, name, usage)
 }
 
-func ParseEnumE[T ~int32](val string) (T, error) {
-	vals := any(T(0)).(protoreflect.Enum).Descriptor().Values()
+func ParseEnumE[T protoreflect.Enum](val string) (T, error) {
+	var t T
+	if v := parseEnum[T](val); v != nil {
+		return t.Type().New(v.Number()).(T), nil
+	} else {
+		return t, fmt.Errorf("unable to parse enum: %s", val)
+	}
+}
+
+func parseEnum[T protoreflect.Enum](val string) protoreflect.EnumValueDescriptor {
+	var t T
+	vals := t.Descriptor().Values()
 	if i, err := strconv.ParseInt(val, 0, 32); err == nil {
-		if n := vals.ByNumber(protoreflect.EnumNumber(i)); n != nil {
-			return T(i), nil
+		if v := vals.ByNumber(protoreflect.EnumNumber(i)); v != nil {
+			return v
 		}
-	} else if n := vals.ByName(protoreflect.Name(val)); n != nil {
-		return T(int32(n.Number())), nil
+	} else if v := vals.ByName(protoreflect.Name(val)); v != nil {
+		return v
 	} else {
 		for i := 0; i < vals.Len(); i++ {
-			if n := vals.Get(i); strings.EqualFold(string(n.Name()), val) {
-				return T(int32(n.Number())), nil
+			if v := vals.Get(i); strings.EqualFold(string(v.Name()), val) {
+				return v
 			}
 		}
 	}
-	return 0, fmt.Errorf("unable to parse enum: %s", val)
+	return nil
 }
